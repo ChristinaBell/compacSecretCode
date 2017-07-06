d9c4a1dc0d55e901aac08351273700e85967acc9
ChristinaBell <cbel296@aucklanduni.ac.nz>
2017-07-07 09:57:27 +1200

    Move AWS call to logging.js file to get working with logs first

--- MapScreen.js
+++ MapScreen.js
@@ -33,36 +33,6 @@ $( document ).ready(function() {
 
     // ----------------------------------------------------------------------------------------------------------------
 
-    // AWS Lambda call
-
-//    AWS.config.update({region: 'ap-southeast-2'});
-//    AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: 'ap-southeast-2:4d0f8a86-6998-44d5-b05b-953269d29426'});
-
-    AWS.config.region = 'ap-southeast-2'; // Region
-    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: 'ap-southeast-2:4d0f8a86-6998-44d5-b05b-953269d29426',
-    });
-
-    var lambda = new AWS.Lambda({region: 'ap-southeast-2', apiVersion: '2015-03-31'});
-    // create JSON object for parameters for invoking Lambda function
-    var pullParams = {
-      FunctionName : 'readLogsFromS3',
-      InvocationType : 'RequestResponse',
-      LogType : 'None'
-    };
-    // create variable to hold data returned by the Lambda function
-    var pullResults;
-
-    lambda.invoke(pullParams, function(error, data) {
-      if (error) {
-        prompt(error);
-      } else {
-        pullResults = JSON.parse(data.Payload);
-        console.log(pullResults);
-      }
-    });
-
-
 
 
 
