67a7e21e20492f6601e4c43e0035735b4fbe5198
ChristinaBell <cbel296@aucklanduni.ac.nz>
2017-07-14 16:57:44 +1200

    Filter working for error type on logging page

--- Logging.js
+++ Logging.js
@@ -1,39 +1,56 @@
 $( document ).ready(function() {
 
+    // Define logging filers
+    var ERRORTYPE = {
+        "ERROR":  "ERROR",
+        "WARNING": "WARNING"
+    }
+
+
     // AWS Lambda call
     AWS.config.region = 'ap-southeast-2'; // Region
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({
         IdentityPoolId: 'ap-southeast-2:4d0f8a86-6998-44d5-b05b-953269d29426',
     });
 
-    // Create Lambda object
-    var lambda = new AWS.Lambda({region: 'ap-southeast-2', apiVersion: '2015-03-31'});
-
-    var filters = {"Machine": ["sizer"] };
-
-    // create JSON object for parameters for invoking Lambda function
-    var pullParams = {
-      FunctionName : 'getLogsFiltered',
-      InvocationType : 'RequestResponse',
-      Payload: JSON.stringify(filters),
-      LogType : 'None'
+    //Call function to update the table with selected filters
+    var filters = {
+        "ErrorType": [ERRORTYPE.ERROR, ERRORTYPE.WARNING],
+        "Machine": ["SIZER"]
     };
+    updateTable(filters);
 
-    // Create variable to hold data returned by the Lambda function
-    var pullResults;
+    function updateTable(filters){
+        // Create Lambda object
 
-    lambda.invoke(pullParams, function(error, data) {
-      if (error) {
-        prompt(error);
-      } else {
-        pullResults = JSON.parse(data.Payload);
-        // Reload table with results from S3 lambda function call
-        reloadTable(pullResults);
-      }
-    });
+        var lambda = new AWS.Lambda({region: 'ap-southeast-2', apiVersion: '2015-03-31'});
+
+        // create JSON object for parameters for invoking Lambda function
+        var pullParams = {
+          FunctionName : 'getLogsFiltered',
+          InvocationType : 'RequestResponse',
+          Payload: JSON.stringify(filters),
+          LogType : 'None'
+        };
+
+        // Create variable to hold data returned by the Lambda function
+        var pullResults;
+
+        lambda.invoke(pullParams, function(error, data) {
+          if (error) {
+            prompt(error);
+          } else {
+            pullResults = JSON.parse(data.Payload);
+//            console.log(pullResults);
+            // Reload table with results from S3 lambda function call
+            reloadTable(pullResults);
+          }
+        });
+    }
 
     // Function to reload logging table with data from S3
     function reloadTable(data){
+        $('#logging-table').bootstrapTable("removeAll");
         for (item in data){
             var obj = JSON.parse(data[item]);
             var date = new Date((item.split("/")[1])*1000);
@@ -48,6 +65,25 @@ $( document ).ready(function() {
             $('#logging-table').bootstrapTable("append", dict);
         }
     }
+
+    $('#update_logs').click(function() {
+        var errorTypes = [];
+
+        if ($('#errorCheckbox').is(":checked")){
+            errorTypes.push(ERRORTYPE.ERROR);
+        }
+        if ($('#warningCheckbox').is(":checked")){
+            errorTypes.push(ERRORTYPE.WARNING);
+        }
+
+//        console.log(errorTypes);
+        filters.ErrorType = errorTypes;
+        updateTable(filters);
+    });
+
+
+
+
 
 
 });
