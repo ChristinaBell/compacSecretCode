6ea1230dc09bdb36ea43af44ead0ef87544d49f0
ChristinaBell <cbel296@aucklanduni.ac.nz>
2017-07-17 14:04:43 +1200

    update defaault date to include data from db

--- Logging.js
+++ Logging.js
@@ -50,7 +50,7 @@ $( document ).ready(function() {
             prompt(error);
           } else {
             pullResults = JSON.parse(data.Payload);
-            console.log(pullResults);
+//            console.log(pullResults);
             // Reload table with results from S3 lambda function call
             reloadTable(pullResults);
           }
