e29e7244a2611bbf4a55b5b7b08ebd3f533f6903
ChristinaBell <cbel296@aucklanduni.ac.nz>
2017-09-19 16:29:14 +1200

    Bug fixed last packhouse not removing from selected customer filter

--- Logging.js
+++ Logging.js
@@ -144,11 +144,9 @@ $(document).ready(function() {
         var html = "<h4 class='log-sub-heading'>Packhouses:</h4>";
         for (var i = 0; i < packhouses.length; i++) {
             var currentPackhouse = packhouses[i];
-
             html = html + "<div class='checkbox'><label><input checked class='packhouse_checkbox' type='checkbox' name='Customer' value='" + currentPackhouse + "'>" + currentPackhouse + " </label> </div>"
-            packhousechecklist.html(html);
         }
-
+        packhousechecklist.html(html);
         getSelectedCustomersAndPackhouses();
         retrieveData(filters);
     }
@@ -222,10 +220,12 @@ $(document).ready(function() {
 
     }
 
-    // Refresh packhouse dropdowns based on which customers are selected. 
+    // Refresh packhouse dropdowns based on which customers are selected.
     $('#customer-check-list').click(function() {
+//        console.log("clicked!");
         getPackhouses();
     });
+
 
 
     // Function to update the log when the update logs button is clicked -----------------------------------------------
